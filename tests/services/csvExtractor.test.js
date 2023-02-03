5eecab9918f895ea16b2dea03677844a11541a7a
Soumil Maitra <soumilmaitra414@gmail.com>
2023-02-03 12:16:45 +0530

    test: create test file for csv extractor services file

--- tests/services/csvExtractor.test.js
+++ tests/services/csvExtractor.test.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const csvServices = require('../../src/services/csvExtractor');
 
+
 describe('Test for checking if csv file is extracted', () => {
   it('should return csv data if end point is correct', async () => {
     jest.spyOn(axios, 'get').mockresolvedValue('fileData');
